bc5215e0ff94f1e945744d98a80ac1cfa1ae8118
lazy-uta <radityauta@gmail.com>
2022-04-24 22:56:35 +0700

    pertemuan 1 - 3

---
+++ pertemuan2/index.php
@@ -0,0 +1,58 @@
+<?php 
+// komentar nih bos 
+// SINTAKS PHP
+
+//standat Output
+// echo, print
+// print_r (cetak isi array)
+// var_dump 
+
+//penulisan sintaks php
+//1. PHP di dalam HTML
+
+//2. Html didalam php
+
+//variable dan tipe data
+// $x = 2;
+// $y = 10;
+
+// echo $x + $y;
+
+// $nama_depan = "Uta";
+// $nama_belakang = "Praditya";
+
+// echo $nama_depan." ".$nama_belakang
+
+//operator assigment 
+
+// $x = 2;
+// $x += 10;
+// echo $x;
+
+// $nama = "Uta";
+// $nama .= " ";
+// $nama .="Praditya";
+
+// echo $nama;
+
+// var_dump(1 == 5);
+
+//indentitas 
+// var_dump(1 === "1");
+
+// operator logika
+// &&, || , !
+
+var_dump(3 == 1 || 3 == "1");
+?>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1> Halo <?php echo $nama;?> </h1> 
+</body>
+</html> -->
---
+++ pertemuan3/index.php
@@ -0,0 +1,23 @@
+<?php
+
+//for 
+// for( $x = 0; $x <= 10; $x++){
+//     echo "oke <br>";
+// }
+
+//while
+// $a = 0;
+
+// while ($a <= 10) {
+//     echo "Oke <br>";
+//     $a++;
+// }
+
+//do while
+$a = 0;
+do {
+    echo "oke <br>";
+    $a++;
+} while ($a <= 10);
+
+?>
